cfdecabcadee6c8ecc9d4c7e5ba2ea0f1a4845cf
abir-Bk <benkhlifaabiir@gmail.com>
2026-02-26 02:31:03 +0100

    fix

--- config/bundles.php
+++ config/bundles.php
@@ -18,5 +18,4 @@ return [
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
     KnpU\OAuth2ClientBundle\KnpUOAuth2ClientBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
-    Scheb\TwoFactorBundle\SchebTwoFactorBundle::class => ['all' => true],
 ];
